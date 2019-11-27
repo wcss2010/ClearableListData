bb5afe93fc6f3996fecc1f51fd16920b332b3aec
wcss <wcss2020@gmail.com>
2019-11-27 15:26:11 +0800

    ...

--- Code/ClearableListDataApp/ClearableListDataApp/MainForm.Designer.cs
+++ Code/ClearableListDataApp/ClearableListDataApp/MainForm.Designer.cs
@@ -28,20 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.SuspendLayout();
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(890, 427);
+            this.ClientSize = new System.Drawing.Size(964, 608);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "兼容性视图导入工具 For IE8,IE9,IE10,IE11";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
     }
 }
 
--- Code/ClearableListDataApp/ClearableListDataApp/MainForm.cs
+++ Code/ClearableListDataApp/ClearableListDataApp/MainForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,9 +11,71 @@ namespace ClearableListDataApp
 {
     public partial class MainForm : Form
     {
+        /// <summary>
+        /// 配置文件路径
+        /// </summary>
+        public static string configFile = System.IO.Path.Combine(Application.StartupPath, "configs.cfg");
+
+        /// <summary>
+        /// 配置对象
+        /// </summary>
+        public static ClearableListObject Config { get; set; }
+
         public MainForm()
         {
             InitializeComponent();
+
+            //载入配置
+            if (System.IO.File.Exists(configFile))
+            {
+                Config = ClearableListObject.fromFile(configFile);
+            }
+            else
+            {
+                Config = new ClearableListObject();
+                Config.WebSiteList.Add("test.com");
+                ClearableListObject.toFile(Config, configFile);
+            }
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            this.Text += "(" + System.IO.Path.GetFileName(configFile) + ")";
+        }
+    }
+
+    /// <summary>
+    /// 兼容性视图地址列表对象
+    /// </summary>
+    public class ClearableListObject
+    {
+        public ClearableListObject()
+        {
+            WebSiteList = new List<string>();
+        }
+
+        public List<string> WebSiteList { get; set; }
+
+        /// <summary>
+        /// 序列化
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="jsonFile"></param>
+        public static void toFile(ClearableListObject obj, string jsonFile)
+        {
+            System.IO.File.WriteAllText(jsonFile, JsonConvert.SerializeObject(obj, Formatting.Indented));
+        }
+
+        /// <summary>
+        /// 反序列化
+        /// </summary>
+        /// <param name="jsonFile"></param>
+        /// <returns></returns>
+        public static ClearableListObject fromFile(string jsonFile)
+        {
+            return JsonConvert.DeserializeObject<ClearableListObject>(System.IO.File.ReadAllText(jsonFile));
         }
     }
 }
