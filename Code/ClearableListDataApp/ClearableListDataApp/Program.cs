f9e74d1c16e61290fa8d002546bcc9ec48f758df
wcss <wcss2020@gmail.com>
2019-11-27 16:07:27 +0800

    ..

--- Code/ClearableListDataApp/ClearableListDataApp/Program.cs
+++ Code/ClearableListDataApp/ClearableListDataApp/Program.cs
@@ -11,8 +11,15 @@ namespace ClearableListDataApp
         /// 应用程序的主入口点。
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+            if (args.Length >= 1)
+            {
+                if (args[0] != null && args[0].EndsWith(".ieviewconfig") && System.IO.File.Exists(args[0]))
+                {
+                    MainForm.configFile = args[0];
+                }
+            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
